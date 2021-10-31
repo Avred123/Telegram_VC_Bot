477dfcdde562d66251b9e8b9202b8acfd870ad96
Avred123 <93434197+Avred123@users.noreply.github.com>
2021-10-31 18:47:34 +0530

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,13 +1,13 @@
 HELP_TEXT = """__**LUMINOUS MUSIC BOT**__
 
 **/skip** __Skip the current music.__
-**/play** __Play music, Usage: /play song_name.__
+**/play** __Play music, **Usage:** /play song_name.__
 **/joinvc** __Join voice chat.__
 **/leavevc** __Leave Voice Chat.__
-**/volume** __Adjust volume, Usage: /volume [1-200].__
+**/volume** __Adjust volume, **Usage:** /volume [1-200].__
 **/pause** __Pause music.__
 **/resume** __Resume music.__
-**/queue plformat/Nothing** __Show queue.__
+**/queue** __Show queue.__
 **/delqueue** __Delete queue.__
 **/playlist** __Start playlist.__
 """
